2470890eee5ae6c22e37d5daf1d3e6bbbad68635
Graand Zenizer <graandzenizer@iPhone-6.localdomain>
2025-05-28 13:49:01 -0400

    added init_db.py to backend/app/db

--- backend/app/db/init_db.py
+++
@@ -1,9 +0,0 @@
-from app.db.database import Base, engine
-from app.models import user, schedule, optimized_schedule
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    init_db()
-    print("Tables created successfully.")
